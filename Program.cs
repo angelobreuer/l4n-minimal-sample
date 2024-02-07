c01847ab4649539b4f82d5de1540991c2e47fac1
Angelo Breuer <angelobr@outlook.de>
2024-02-07 19:21:38 +0100

    feat: Initial Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Discord.Interactions;
+using Discord.WebSocket;
+using Lavalink4NET.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MinimalBoilerplate;
+
+var builder = new HostApplicationBuilder(args);
+
+// Discord
+builder.Services.AddSingleton<DiscordSocketClient>();
+builder.Services.AddSingleton<InteractionService>();
+builder.Services.AddHostedService<DiscordClientHost>();
+
+// Lavalink
+builder.Services.AddLavalink();
+builder.Services.AddLogging(x => x.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+builder.Build().Run();
